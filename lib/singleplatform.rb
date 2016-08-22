aae3c82aef1e1a2c007cd624bbb6d8e27dc848d8
Jeff Gharakhanian <jg@okayjeff.com>
2016-08-22 13:04:38 -0400

    Add alias for instantiating API and Organize into gem folder structure

---
+++ lib/singleplatform.rb
@@ -0,0 +1,15 @@
+require_relative 'singleplatform/client'
+
+module Singleplatform
+
+  class << self
+
+    # Alias for Singleplatform::Client.new
+    #
+    # @return [Singleplatform::Client]
+
+    def new
+      Singleplatform::Client.new
+    end
+  end
+end
