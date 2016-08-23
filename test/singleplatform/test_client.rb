1d5439e75881b249fdf075ac5e7b7df4949d9081
Jeff G <jg@okayjeff.com>
2016-08-23 06:13:27 -0400

    Add client test file

---
+++ test/singleplatform/test_client.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'singleplatform'
+
+class Singleplatform::ClientTest < Minitest::Test
+  def test_urls_generate_correctly
+    skip
+    # path   = '/locations/updated_since/'
+    # params = { date: '2016-08-01', limit: 10 }
+    # url = generate_url(path, params)
+    # Assert URL is what is should be...
+  end
+end
