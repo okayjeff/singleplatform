d4739af801f57cd978156a2503878c57a926cb9d
Jeff G <jg@okayjeff.com>
2016-08-27 12:25:31 -0400

    Remove test gem

--- test/test_singleplatform.rb
+++ test/test_singleplatform.rb
@@ -3,7 +3,10 @@ require 'singleplatform'
 
 class SingleplatformTest < Minitest::Test
   def test_new_creates_new_client_object
-    client = Singleplatform.new
+    client = Singleplatform.new(
+      client_id:     'purplespacesuitfrogboots1',
+      client_secret: 'yellowsubmarinesresonatewithmeandmybestbros'
+    )
     assert_instance_of(Singleplatform::Client, client)
   end
 end
