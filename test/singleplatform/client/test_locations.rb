eb77622d8d82b6fbc8400ff071f3e522aad17232
Jeff G <jg@okayjeff.com>
2016-08-27 14:05:35 -0400

    WIP test for Locations#locations_updated_since

--- test/singleplatform/client/test_locations.rb
+++ test/singleplatform/client/test_locations.rb
@@ -3,5 +3,18 @@ require 'webmock/minitest'
 require 'singleplatform'
 
 class Singleplatform::Client::LocationsTest < Minitest::Test
-
+  def test_updated_since_fetches_locations
+    url = /SinglePlatform::Client::BASE_URL\/locations\/updated_since*/
+    response = File.read('test/fixtures/updated_since_response.json')
+    stub_request(:get, url).
+      with(
+        body: response,
+        headers: { "Content-Type" => 'application/json' }
+      )
+    client = Singleplatform::Client.new(
+      client_id:     'purplespacesuitfrogboots1',
+      client_secret: 'yellowsubmarinesresonatewithmeandmybestbros'
+    )
+    client.locations_updated_since(date: '2012-11-16', limit: 11)
+  end
 end
