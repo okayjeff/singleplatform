71040f4969267d15d6b96b87ffb02b056e9fba77
Jeff G <jg@okayjeff.com>
2016-08-28 20:04:19 -0400

    Clean up comments

--- lib/singleplatform/client/locations.rb
+++ lib/singleplatform/client/locations.rb
@@ -4,8 +4,9 @@ module Singleplatform
       # Fetch information about a specific location
       #
       # @param id [String]
-      # @param options [Hash] :format Short menu ('short') available
-      # @return [Hash]
+      # @param options [Hash]
+      # @option options [String] :format Short menu ('short') available
+      # @return [Hashie::Mash]
       def location(id, options = {})
         url = generate_url("/locations/#{id}/", options)
         Request.get(url)
@@ -14,8 +15,9 @@ module Singleplatform
       # Fetch all locations updated since a given date
       #
       # @param date [String]
-      # @param options [Hash] :limit Maximum (default) 5000 per page
-      # @return [Hash]
+      # @param options [Hash]
+      # @option options [Fixnum] :limit Maximum (default) 5000 per page
+      # @return [Hash::Mash]
       def locations_updated_since(date, options = {})
         url = generate_url(
           '/locations/updated_since/',
