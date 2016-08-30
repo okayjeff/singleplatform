d1589f43acb9763d6096399f08cca41ae85abb05
Jeff G <jg@okayjeff.com>
2016-08-29 20:55:38 -0400

    Response#next wip

--- lib/singleplatform/response.rb
+++ lib/singleplatform/response.rb
@@ -1,15 +1,22 @@
 module Singleplatform
   class Response
-    attr_accessor :code, :body, :next_url
+    attr_accessor :code, :body, :next_page
 
     def initialize(args)
-      @code = args[:code]
-      @body = args[:body]
-      @next_url = args[:body].next
+      @code      = args[:code]
+      @body      = args[:body]
+      @next_page = args[:body].next
     end
 
     def next
+      return nil if next_page.nil?
+      path = strip_base_url(next_page)
+      path << "&client=#{ENV['CLIENT_ID']}"
       
+    end
+
+    def strip_base_url(url)
+      url.slice!(/http:\/\/publishing-api.singleplatform.com/)
     end
   end
 end
