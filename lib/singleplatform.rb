f7c6a6ece3be0a17526d3c3d8aecb8f5acf99ad4
Jeff G <jg@okayjeff.com>
2016-09-01 16:35:12 -0400

    Fix style violations

--- lib/singleplatform.rb
+++ lib/singleplatform.rb
@@ -1,15 +1,12 @@
 require 'singleplatform/client'
 
 module Singleplatform
-
   class << self
-
     # Alias for Singleplatform::Client.new
     #
     # @return [Singleplatform::Client]
     def new(args = {})
       Singleplatform::Client.new(args)
     end
-
   end
 end
