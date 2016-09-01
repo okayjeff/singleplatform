3ec5fc228c212f9bab95d4b5a0738a6b16fdcf7b
Jeff G <jg@okayjeff.com>
2016-09-01 12:53:27 -0400

    Support all endpoint in Singleplatform::Client::Locations

--- lib/singleplatform/client/locations.rb
+++ lib/singleplatform/client/locations.rb
@@ -6,9 +6,17 @@ module Singleplatform
       # @param id [String]
       # @param options [Hash]
       # @option options [String] :format Short menu ('short') available
-      # @return [Hashie::Mash]
+      # @return [Singleplatform::Response]
       def location(id, options = {})
         url = generate_url("/locations/#{id}/", options)
+        Request.get(url)
+      end
+
+      # Fetch location, menus and photo data for a specific location
+      #
+      # @return [Singleplatform::Response]
+      def all_for(id)
+        url = generate_url("/locations/#{id}/all")
         Request.get(url)
       end
 
@@ -17,7 +25,7 @@ module Singleplatform
       # @param date [String]
       # @param options [Hash]
       # @option options [Fixnum] :limit Maximum (default) 5000 per page
-      # @return [Hashie::Mash]
+      # @return [Singleplatform::Response]
       def locations_updated_since(options = {})
         url = generate_url('/locations/updated_since/', options)
         Request.get(url)
