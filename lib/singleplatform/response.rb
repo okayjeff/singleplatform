49d4bf619bf5f0ee3b49cb7dc0cb08688852afa0
Jeff G <jg@okayjeff.com>
2016-09-01 10:24:43 -0400

    Implement draft version of Response#next

--- lib/singleplatform/response.rb
+++ lib/singleplatform/response.rb
@@ -10,11 +10,18 @@ module Singleplatform
       @next_page = args[:body].next
     end
 
+    # An iterator for retrieving the next page of results from
+    # API response.
+    #
+    # @note Will only work with Client#locations_updated_since as
+    # it's the only paginated response in the API at this time.
+    #
+    # @return [Hashie::Mash]
     def next
       return nil if next_page.nil?
       params = parse_params(next_page)
-      params['client'] ||= ENV['CLIENT_ID']
-      client = Singleplatform.new(
+      params['client'] = ENV['CLIENT_ID']
+      client ||= Singleplatform.new(
         client_id:     ENV['CLIENT_ID'],
         client_secret: ENV['CLIENT_SECRET']
       )
